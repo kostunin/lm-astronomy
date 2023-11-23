5008b38025553b69d1a0aefb5c71cc23310a3e0d
Dmitriy Kostunin <dmitriy.kostunin@desy.de>
2023-11-23 12:37:12 +0100

    adding script for checking coordinates

--- src/api/test_coordinates.py
+++ src/api/test_coordinates.py
@@ -10,19 +10,24 @@ count_broken = 0
 count_empty = 0
 list_broken = []
 list_empty = []
+list_single_obj = []
 
 with open(path, 'r') as f:
     raw_records = json.load(f)
     for name, data in raw_records.items():
         try:
-            if len(RecordMetadata(record_id=name, **data).skycoord) == 0:
+            md = RecordMetadata(record_id=name, **data)
+            if len(md.skycoord) == 0:
                 count_empty += 1
                 list_empty.append(name)
         except ValidationError as e:
             print(name, e)
             count_broken += 1
             list_broken.append(name)
+        if len(md.object_name) == 1 and len(md.skycoord) == 1:
+            list_single_obj.append(md)
 
 
 print(count_broken, count_empty)
 print(list_broken)
+print(len(list_single_obj))
